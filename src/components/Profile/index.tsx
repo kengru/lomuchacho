7df35f27f72a09266309728f3476d6939d09d8b3
kengru <kengrullon@gmail.com>
2019-06-29 10:21:09 -0400

    Added useState to profile.

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface ProfileProps {
   userName: string
 };
 
 const Profile: React.FC<ProfileProps> = props => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>{props.userName}</div>
+    <div onClick={() => setCount(count + 1)}>
+      {props.userName}, {count}
+    </div>
   );
 }
+
+export default Profile;
