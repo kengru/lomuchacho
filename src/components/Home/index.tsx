fa0cb13c9d57c00ff2f7b32814747f3a4d14f1a6
kengru <kengrullon@gmail.com>
2019-07-01 17:35:59 -0400

    Adding media querys to make it a bit user friendly.

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -26,6 +26,13 @@ const Home: React.FC = () => {
           <h4>Steven Catalino</h4>
           <p>Este pana x hace x y luego x klk</p>
         </Card>
+        <Card interactive elevation={Elevation.TWO} className="profileCard">
+          <div className="Still">
+            <img src={`https://firebasestorage.googleapis.com/v0/b/kengru-do.appspot.com/o/losmuchachos%2FDSC_0799%20(2).jpg?alt=media&token=0ee583b5-a3e2-47f2-aa21-86d25f7c7566`} />
+          </div>
+          <h4>Steven Catalino</h4>
+          <p>Este pana x hace x y luego x klk</p>
+        </Card>
       </div>
     </div>
   );
