5d8b89c0fa8fc37d6526a5b1a9fea711f75e4062
kengru <kengrullon@gmail.com>
2019-07-01 17:05:06 -0400

    Added blueprint, creating profile cards.

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
+import { Button, Card, Elevation } from "@blueprintjs/core";
+import "./index.css";
 
 const Home: React.FC = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <div onClick={() => setCount(count + 1)}>
-      ken, {count}
+    <div className="home">
+      <h1 className="bp3-heading">
+        Los Muchachos
+      </h1>
+      <Button icon="database" large>Eventos</Button>
+      <div className="profileList">
+        <Card interactive elevation={Elevation.TWO} className="profileCard">
+          <div className="Still">
+            <img src={`https://firebasestorage.googleapis.com/v0/b/kengru-do.appspot.com/o/losmuchachos%2FDSC_0799%20(2).jpg?alt=media&token=0ee583b5-a3e2-47f2-aa21-86d25f7c7566`} />
+          </div>
+          <h4>Leudy Lopez</h4>
+          <p>Este pana x hace x y luego x klk</p>
+        </Card>
+        <Card interactive elevation={Elevation.TWO} className="profileCard">
+          <div className="Still">
+            <img src={`https://firebasestorage.googleapis.com/v0/b/kengru-do.appspot.com/o/losmuchachos%2FDSC_0799%20(2).jpg?alt=media&token=0ee583b5-a3e2-47f2-aa21-86d25f7c7566`} />
+          </div>
+          <h4>Steven Catalino</h4>
+          <p>Este pana x hace x y luego x klk</p>
+        </Card>
+      </div>
     </div>
   );
 }
