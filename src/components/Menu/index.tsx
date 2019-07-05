d36252339191c8de07116cd9692ca3e4185648bc
Kendry Grullon <kengrullon@gmail.com>
2019-07-05 15:34:21 -0400

    Adding side menu without styling.

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Menu: React.FC = () => {
+  return (
+    <aside className="menu">
+      <p className="menu-label">
+        Guys
+      </p>
+      <ul className="menu-list">
+        <li>Kendry</li>
+        <li>Leudy</li>
+        <li>Steven</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Menu;
--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,44 +1,25 @@
 import React from "react";
-// import { Switch, Route } from "react-router-dom";
-// import Profile from "../../components/Profile";
-// import Home from "../../components/Home";
+import { Switch, Route } from "react-router-dom";
+
+import Menu from "../../components/Menu";
+import Profile from "../../components/Profile";
+import Home from "../../components/Home";
 
 import "./App.sass";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      {/* <Switch>
-        <Route path="/profile/:user" component={Profile}/>
-        <Route path="/" component={Home} />
-      </Switch> */}
-      <h1 className="title">Bulma</h1>
-      <p className="subtitle">
-        Modern CSS framework based on{" "}
-        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
-          Flexbox
-        </a>
-      </p>
-
-      <div className="field">
-        <div className="control">
-          <input className="input" type="text" placeholder="Input" />
+      <div className="columns">
+        <div className="column is-one-third">
+          <Menu />
         </div>
-      </div>
-
-      <div className="field">
-        <p className="control">
-          <span className="select">
-            <select>
-              <option>Select dropdown</option>
-            </select>
-          </span>
-        </p>
-      </div>
-
-      <div className="buttons">
-        <a className="button is-primary">Primary</a>
-        <a className="button is-link">Link</a>
+        <div className="column">
+          <Switch>
+            <Route path="/profile/:user" component={Profile}/>
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
       </div>
     </div>
   );
